476f1e7ce424fb2408c162794767ee369e89c22b
Dr. Sarveshwar Bharti <sarveshwar.csc@gndu.ac.in>
2025-11-08 15:58:04 +0530

    Update and rename HelloServlet.java to HelloServletGet.java

--- Java Servlets/HelloServletGet.java
+++ Java Servlets/HelloServletGet.java
@@ -3,7 +3,7 @@ import java.io.*;
 import javax.servlet.*; 
 import javax.servlet.http.*;
 
-public class HelloServlet extends HttpServlet {
+public class HelloServletGet extends HttpServlet {
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     
